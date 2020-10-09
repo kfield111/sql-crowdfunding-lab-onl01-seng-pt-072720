ef25680aff513383e97ee73ae0841c90a9d8a1a0
Kelin Field <kfield111@hotmail.com>
2020-10-09 20:46:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,8 +8,8 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount)
-  FROM pledges
-  INNER JOIN projects
+  FROM projects
+  INNER JOIN pledges
   ON pledges.project_id = projects.id;
   GROUP BY projects.id;
   GROUP BY projects.title"
